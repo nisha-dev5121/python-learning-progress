9a239784d713ff408973a38d7634575abac1c2ae
NISHA <nisha.dev5121@gmail.com>
2026-03-31 19:59:18 +0530

    Delete CHAPTER03 directory

--- CHAPTER03/p01.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ")
-print("Good Afternoon, " + name)   #this will print "Good Afternoon " followed by the name entered by the user
-print("Good Afternoon, " + name + " how are you?")   #this will print "Good Afternoon " followed by the name entered by the user and then " how are you?"
-print(f"Good Afternoon, {name}")   #this will print "Good Afternoon " followed by the name entered by the user using f-string
--- CHAPTER03/p05.py
+++
@@ -1,2 +0,0 @@
-letter = "Dear Nisha,\n\tThis python course is nice.\nThanks!"
-print(letter)
