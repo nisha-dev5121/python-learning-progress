16953f8597a57e2a4f8a1fc23f9e11a993519372
NISHA <nisha.dev5121@gmail.com>
2026-03-31 19:59:27 +0530

    Delete CHAPTER04 directory

--- CHAPTER04/tuple_method.py
+++
@@ -1,9 +0,0 @@
-a = (1, 34, 67, 86, "rohan", False, 34)
-print(a)
-
-no = a.count(34)
-print(no)
-
-i = a.index(34)
-print(i)
-# it occurs 2 times but the code will only give the index no on which it will find that value for the first time.
