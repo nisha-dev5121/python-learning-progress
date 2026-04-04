3f7a404fafce084d405e83197e9694eea9401f68
NISHA <nisha.dev5121@gmail.com>
2026-04-04 14:44:47 +0530

    Delete CHAPTER06-P directory

--- CHAPTER06-P/problem04.py
+++
@@ -1,5 +0,0 @@
-user_name = input("Enter your username:")
-if len(user_name) < 10 :
-    print("Your username is less than 10 characters.  ")
-else :
-    print("Your username is greater than or equal to 10 characters.  ")
